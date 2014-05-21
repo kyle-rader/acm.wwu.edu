027b7e49360fb93c0c6ab36c3978fa44125f45e1
kyle-rader <kyle.rader@ieee.org>
2014-05-21 10:00:16 -0700

    Format voting page

--- vote.php
+++ vote.php
@@ -1,21 +1,45 @@
 <?php 
 include_once('php_src/CAS-1.3.2/CAS.php');
 
+$baseInclude = $_SERVER['DOCUMENT_ROOT'] . '/include';
+$page_title = "WWU CS Proff of the year";
+
+include_once "$baseInclude/functions.inc";
+
 phpCAS::client(CAS_VERSION_2_0, 'websso.wwu.edu', 443, '/cas');
 phpCAS::setNoCasServerValidation();
 phpCAS::forceAuthentication();
+
 if(isset($_REQUEST['logout'])) {
-    phpCAS::logout();
+    phpCAS::logoutWithRedirectService('http://acm.wwu.edu/thankyou.php');
 }
+
+$title = <<<EOT
+			<h2>WWU CS Professor of the year 13-14</h2>
+			<small></small>
+EOT;
 ?>
+
+<!DOCTYPE html>
 <html>
-        <head>
-                <title>phpCAS simple client</title>
-        </head>
-        <body>
-                <h1>Successfull Authentication!</h1>
-                <p>The user's login is <strong><?php echo phpCAS::getUser(); ?></strong></p>
-                <p>The phpCAS version is <strong><?php echo phpCAS::getVersion(); ?></strong></p>
-                <p><a href="?logout=">Logout</a></p>
-        </body>
+	<?php include_once "$baseInclude/header.inc"; ?>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+			</div>
+			<div id="content">
+				<?php print PageTitle($title); ?>
+				<div class="row">
+					<div class="large-12 columns">
+						<h4>You have Successfully Authenticated.</h4>
+						You are voting as <strong><?php echo phpCAS::getUser(); ?></strong><br>
+						<small>(If this is not you, please <a href="?logout">logout</a> and log back in as you!)</small>
+					</div>
+				</div>
+			</div>
+			<div id="footer">
+				<?php include_once "$baseInclude/footer.inc"; ?>
+			</div>
+		</div>
+	</body>
 </html>
